3947859fed152c18b570ffc7b6ccbc8b45a71ba7
okuninoshi <okuninoshi@gmail.com>
2020-09-26 21:09:05 +0200

    add borders to nav links

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,6 +36,8 @@ const GlobalStyle = createGlobalStyle`
     }
 `
 const H4 = styled.h4`
+  font-family:${props => props.theme.fonts.secondary};
+  font-size:calc(${props => props.theme.golden_ratio}px * 21);
   color:${props => (props.isDark ? props.theme.dark.text : props.theme.palette.secondary)}
 `
 
@@ -50,6 +52,46 @@ const RetroScreen = styled.div`
   pointer-events: none;
   z-index:200;
   filter: opacity(10%);
+`
+const NavContainer = styled.div`
+    position:relative;
+    padding:16px;
+    .top_left_corner {
+      position:absolute;
+      top:-10px;
+      left:-10px;
+      width:10px;
+      height:10px;
+      border-top:1px solid ${props => (props.isDark ? props.theme.dark.text : props.theme.palette.secondary)};
+      border-left:1px solid ${props => (props.isDark ? props.theme.dark.text : props.theme.palette.secondary)};
+    }
+    .top_right_corner {
+      position:absolute;
+      top:-10px;
+      right:-10px;
+      width:10px;
+      height:10px;
+      border-top:1px solid ${props => (props.isDark ? props.theme.dark.text : props.theme.palette.secondary)};
+      border-right:1px solid ${props => (props.isDark ? props.theme.dark.text : props.theme.palette.secondary)};
+    }
+    .bottom_right_corner {
+      position:absolute;
+      bottom:-10px;
+      right:-10px;
+      width:10px;
+      height:10px;
+      border-bottom:1px solid ${props => (props.isDark ? props.theme.dark.text : props.theme.palette.secondary)};
+      border-right:1px solid ${props => (props.isDark ? props.theme.dark.text : props.theme.palette.secondary)};
+    }
+    .bottom_left_corner {
+      position:absolute;
+      bottom:-10px;
+      left:-10px;
+      width:10px;
+      height:10px;
+      border-bottom:1px solid ${props => (props.isDark ? props.theme.dark.text : props.theme.palette.secondary)};
+      border-left:1px solid ${props => (props.isDark ? props.theme.dark.text : props.theme.palette.secondary)};
+    }
 `
 
 
@@ -80,8 +122,15 @@ const Layout = ({ children }) => {
           </Col>
         </Row>
         <Row className="d-flex justify-content-center">
-          <Link className="mx-2 py-3" to={"/posts"}>Posts</Link>
-          <Link className="mx-2 py-3" to={"/connect"}>connect</Link>
+          <NavContainer>
+            <div className="top_left_corner"></div>
+            <div className="top_right_corner"></div>
+            <div className="bottom_right_corner"></div>
+            <div className="bottom_left_corner"></div>
+            <Link className="mx-2 py-3" to={"/posts"}>Posts</Link>
+            <Link className="mx-2 py-3" to={"/connect"}>connect</Link>
+          </NavContainer>
+
         </Row>
       </Container>
       {children}
