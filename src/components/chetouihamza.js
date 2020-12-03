154462c43fbe780875ad863d13e0233d28a7d0f6
okuninoshi <gennesis@outlook.fr>
2020-12-03 16:52:43 +0100

    add logo

--- src/components/chetouihamza.js
+++ src/components/chetouihamza.js
@@ -1,24 +1,121 @@
 import React from 'react'
 import styled from 'styled-components'
-// import { motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { Link } from 'gatsby'
 
+// 2,69312169312
+const LogoWrapper = styled(motion.span)`
+  #svg1877{
+    height:29px;
+  }
 
-const Logo = styled.h3`
-    font-size:24px;
-    color: #d6f756;
-    padding:0;
-    margin:0;
-    letter-spacing:2.3px;
 `
 
 
 const Chetouihamza = () => {
     return (
         <Link to={"/"}>
-            <Logo >
-                Chetoui Hamza
-            </Logo>
+            <LogoWrapper >
+
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    id="svg1877"
+                    version="1.1"
+                    viewBox="0 0 134.8 50.198"
+                    preserveAspectRatio="xMidYMid meet"
+                >
+                    <g id="layer1" transform="translate(-38.174 -112.489)">
+                        <g
+                            id="g2517"
+                            strokeLinecap="butt"
+                            strokeLinejoin="miter"
+                            strokeOpacity="1"
+                        >
+                            <path
+                                id="path1810"
+                                fill="#684656"
+                                fillOpacity="1"
+                                stroke="none"
+                                strokeWidth="1"
+                                d="M177.012 425.906l-32.733 56.467v56.918h80.565l-8.555 11.338v64.252h72.01l45.96-25.104-15.296 25.104H569.79l41.22-34.975v-47.91l42-36.09-28-14v-25.24l13.516-8.385 14.485-8.375-28 .004v-.004l-14-14-14.022 8.145-13.978-8.145-27.975 16.076-14.025-16.076-25.487 14.024-2.513-.024-28-14-23.989 13.772-31.464.092.199 17.853-.713.41-22.912-32.127h-19.121l-14.274 8.334-55.726-8.334-17.108 10.48-10.892-10.48-29.407 17.451-12.593-17.45-15.256 8.929z"
+                                transform="scale(.26458)"
+                            ></path>
+                            <g
+                                id="g2493"
+                                fill="#fff"
+                                stroke="#684656"
+                                strokeDasharray="none"
+                                strokeMiterlimit="4"
+                                strokeWidth="0.397"
+                            >
+                                <path
+                                    id="path16"
+                                    d="M46.834 112.687v18.521H61.651v-3.704H50.538v-11.112H61.651v-3.705H50.538z"
+                                ></path>
+                                <path
+                                    id="path16-93"
+                                    d="M65.355 112.687v18.521h3.704V123.8H76.468v7.408h3.704v-18.521h-3.704V120.096H69.059V112.687z"
+                                ></path>
+                                <path
+                                    id="path16-12"
+                                    d="M83.876 112.687v18.521h14.817v-3.704H87.58V123.8h7.408v-3.704H87.58v-3.704h11.113v-3.705H87.58z"
+                                ></path>
+                                <path
+                                    id="path16-56"
+                                    d="M102.397 112.687v3.705h7.408v14.816h3.704v-14.816H120.918v-3.705H106.1z"
+                                ></path>
+                                <path
+                                    id="path16-31"
+                                    d="M128.326 112.687v3.705h7.408v-3.705h-3.704zm7.408 3.705v11.112h3.704v-11.112zm0 11.112h-7.408v3.704h7.408zm-7.408 0v-11.112h-3.704v11.112z"
+                                ></path>
+                                <path
+                                    id="path16-22"
+                                    d="M143.143 112.687V127.504h3.704v-14.817zm3.704 14.817v3.704h7.408v-3.704h-3.704zm7.408 0h3.704v-14.817h-3.704V123.8z"
+                                ></path>
+                                <path
+                                    id="path16-73"
+                                    d="M161.663 112.687v3.705h3.705v11.112h-3.705v3.704H172.776v-3.704h-3.704v-11.112h3.704v-3.705h-7.408z"
+                                ></path>
+                            </g>
+                            <g
+                                id="g2500"
+                                fill="#fff"
+                                stroke="#684656"
+                                strokeDasharray="none"
+                                strokeMiterlimit="4"
+                            >
+                                <path
+                                    id="path505"
+                                    strokeWidth="0.397"
+                                    d="M65.355 134.912v18.521h3.704v-7.408H76.468v7.408h3.704v-18.521h-3.704V142.321H69.059V134.912z"
+                                ></path>
+                                <path
+                                    id="path16-638"
+                                    strokeWidth="0.397"
+                                    d="M83.876 134.912v18.521h3.704v-7.408h7.408v7.408h3.705v-18.521H87.58zm3.704 3.705h7.408v3.704H87.58z"
+                                ></path>
+                                <path
+                                    id="path16-38"
+                                    strokeWidth="0.397"
+                                    d="M102.397 134.912v18.521h3.704v-14.816h3.704v14.816h3.704v-14.816h3.704v14.816h3.705v-18.521H106.1z"
+                                ></path>
+                                <path
+                                    id="path16-128"
+                                    strokeWidth="0.397"
+                                    d="M124.622 134.912v3.705h11.112v3.704h3.704v-3.704h3.705v-3.705H128.326zm11.112 7.409h-3.704v3.704h3.704zm-3.704 3.704h-3.704v3.704h-3.704v3.704h18.521v-3.704H132.03z"
+                                ></path>
+                                <path
+                                    id="path16-311"
+                                    strokeWidth="0.265"
+                                    d="M146.847 134.912v18.521h3.704v-7.408h7.408v7.408h3.704v-18.521h-11.112zm3.704 3.705h7.408v3.704h-7.408z"
+                                ></path>
+                            </g>
+                        </g>
+                    </g>
+                </svg>
+
+
+            </LogoWrapper>
         </Link>
     )
 }
