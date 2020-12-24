3e1c0430bc29b5045ab6f560a89bb3c1905f92df
okuninoshi <gennesis@outlook.fr>
2020-12-24 12:12:31 +0100

    change require to import

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,8 +10,8 @@ import { ThemeProvider } from 'styled-components'
 import Theme from './src/themes/theme'
 import GlobalStateProvider from './src/store/globalStateProvider'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'prismjs/themes/prism-custom.css'
 // require("prismjs/themes/prism-okaidia.css")
-require("prismjs/themes/prism-custom.css")
 
 
 export const wrapRootElement = ({ element }) => {
